3c0b06931c1dd6c064226cc993a63e6f6ea4e620
winthesky <winthesky@163.com>
2017-11-12 00:44:07 +0800

    spring boot test

---
+++ src/main/java/com/maxj/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.maxj.controller;
+
+import com.maxj.entity.User;
+import com.maxj.service.impl.UserService;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("/hello")
+    public String hello(Map<String, Object> model, String name) {
+        User user = userService.findUser(name);
+        if (user != null) {
+            model.put("user", user);
+        }
+        return "hello";
+    }
+
+}
