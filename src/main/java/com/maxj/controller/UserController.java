ed5a9ecd5505dfcf53526a5858c215a338551a3f
mxj <winthesky@163.com>
2017-11-18 17:45:12 +0800

    test1

--- src/main/java/com/maxj/controller/UserController.java
+++ src/main/java/com/maxj/controller/UserController.java
@@ -2,24 +2,29 @@ package com.maxj.controller;
 
 import com.maxj.entity.User;
 import com.maxj.service.impl.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/hello")
-    public String hello(Map<String, Object> model, String name) {
+    @RequestMapping("/hello-data")
+    public List<Map<String, Object>> hello(Map<String, Object> model, String name) {
+        List<Map<String, Object>> result = new ArrayList<Map<String, Object>>();
         User user = userService.findUser(name);
         if (user != null) {
             model.put("user", user);
         }
-        return "hello";
+        return result;
     }
 
 }
